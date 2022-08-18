7e56cd8327d373794d296873059af0ca5188727f
mituljain011 <111493326+mituljain011@users.noreply.github.com>
2022-08-18 14:08:39 +0530

    calc

---
+++ main.py
@@ -0,0 +1,18 @@
+print ('Hello World')
+a=int(input("Enter 1st no"))
+b=int(input("Enter 2nd number"))
+c=int(input("Enter choice"))
+if c==1:    
+    print(a+b)
+elif c==2:   
+    print(a-b)
+elif c==3:   
+    print(a*b)
+elif c==4:    
+    print(a/b)
+elif c==5:   
+    print(a%b)
+elif c==6:   
+    print(a**b)
+else:        
+    print("bye")
